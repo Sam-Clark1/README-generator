87e0b7e9ab46408283412492e73dd1e8c510aa0d
Sam Clark <samclark2399@gmail.com>
2022-03-01 23:05:52 -0500

    added full list of questions and way for those to be transported as data to other file.

--- index.js
+++ index.js
@@ -1,13 +1,156 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const { generateMarkdown } = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = data => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter project title.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Give a description of your project. (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter project description.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Give a description of how to install this project to be used. (Required)',
+            validate: installInput => {
+                if (installInput) {
+                    return true;
+                } else {
+                    console.log('Please enter description of installing project.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Give a description of how to use your project. (Required)',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please enter description of how to use project.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmContribute',
+            message: 'Are you open to contributions? If no, will display "Not looking for contributions at this time".',
+            default: true
+          },
+          {
+            type: 'input',
+            name: 'contribute',
+            message: 'Give a description of how others can contribute to your project. (Required)',
+            when: ({ confirmContribute }) => confirmContribute, 
+            validate: contributeInput => {
+                if (contributeInput) {
+                    return true;
+                } else {
+                    console.log('Please enter description of how you want others to contribute to your project.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmTests',
+            message: 'Do you have ways to test your project? If no, will display "Currently no developed ways to test this project".',
+            default: true
+          },
+          {
+            type: 'input',
+            name: 'tests',
+            message: 'Give a description of how others can test your project. (Required)',
+            when: ({ confirmTests }) => confirmTests, 
+            validate: testInput => {
+                if (testInput) {
+                    return true;
+                } else {
+                    console.log('Please enter description of how to test your project.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub username for contact info. (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter GitHub username.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email for contact info. (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter email.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmLicense',
+            message: 'Does your project utilize a license?',
+            default: true
+          },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose desired license.",
+            choices: ["MIT", "Apache", ""],
+            when: ({ confirmLicense }) => confirmLicense
+          },
+    ])
+    .then(data => {
+        console.log(data)
+        return data
+    })
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+questions() 
+    .then(data => {
+        return generateMarkdown(data);
+    })
+    // .then()
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
+// link below used to create badge
+{/* <img src="https://img.shields.io/badge/<label>-<message>-<color>" alt="badge"></img> */}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+
+# ${data.title}
+
+## Description
+${data.description}
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+
+## Installation
+${data.install}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contribute}
+
+## Tests
+${data.tests}
+
+## Questions
+${data.github}
+${data.email}
+
+## License
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown};
